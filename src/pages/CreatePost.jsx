ac0c4a80b65ad65c27449842312159058d1650a8
Kat Schmidt <katschmidt201@gmail.com>
2024-03-18 18:59:23 -0400

    finished base front end

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Alert } from '@mui/material';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [description, setDescription] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
+
+  //React quill settings
+  const modules = {
+    toolbar: [
+      [{'header': [1,2,3,4,5,6, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  }
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ];
+
+
+  const POST_CATEGORIES = [
+    "Blog", "App", "Programming"
+  ];
+
   return (
-    <div>CreatePost</div>
+    <section className="create-post">
+      <div className="container">
+        <h2>Create Post</h2>
+          <Alert severity='error'>This is an error message</Alert>
+          <br/>
+          <form action="" className="form create-post-form">
+            <input type="text" placeholder='Title' value={title} onChange={e => setTitle(e.target.value)}/>
+            <select name="category" value={category} onChange={e => setCategory(e.target.value)} id="">
+              {
+                POST_CATEGORIES.map(category => <option key={category}>{category}</option>)
+              }
+            </select>
+            <ReactQuill modules={modules} formats={formats} value={description} onChange={setDescription}/>
+            <input type="file" onChange={e => setThumbnail(e.target.files[0])} accept='png, jpg, jpeg'/>
+          </form>
+          <br/>
+          <button type="submit" className='btn-register'>Create Post</button>
+      </div>
+    </section>
   )
 }
 
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,33 +1,54 @@
 import React, { useState } from 'react'
 
-import userAvater from '../userAvatar.png';
-
-const userData = [
-  {id: 1, avatar: userAvater, name: 'Kat', posts: 3}
-]
+import userAvatar from '../userAvatar.png';
+import { Link } from 'react-router-dom';
+import { Alert } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
 
 const UserProfile = () => {
-  const [user, setUser] = useState(userData);
+  const [avatar, setAvatar] = useState(userAvatar);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
   return (
-    <section className="user">
-      {user.length > 0 ? <div className='container user-container'>
-        {
-          user.map(({id, avatar, name, posts}) => {
-            return(
-              <>
-              <div className='user-avatar'>
-                <img src={avatar} alt={name} />
-              </div>
-              <div className="user-info">
-                <h4>{name}</h4>
-                <p>{posts}</p>
-              </div>
-              </>
-            )
-          })
-        }
-      </div>:<h2 className='center'>No user found</h2>}
+    <section className="profile">
+      <div className="counter profile-container">
+        <div className="profile-details">
+          <div className="avatar-wrapper">
+            <div className="profile-avatar">
+              <img src={userAvatar} alt="" />
+            </div>
+            {/* Form to update avatar */}
+            <form action="" className="avatar-form">
+              <input 
+                type="file" 
+                name="avatar" 
+                id="avatar"
+                onChange={e => setAvatar(e.target.files[0])} 
+                accept='png, jpg, jpeg'/>
+              <label htmlFor="avatar"><EditIcon /></label>
+            </form>
+            <button className="profile-avatar-btn"><CheckIcon/></button>
+          </div>
+
+          <h1>Kat</h1>
+
+          {/* form to update user details */}
+          <form action="" className="form profile-form">
+            <Alert severity='error'>This is an error message</Alert>
+            <input type="text" placeholder='Full Name' value={name} onChange={e => setName(e.target.value)}/>
+            <input type="email" placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
+            <input type="password" placeholder='Current Password' value={currentPassword} onChange={e => setCurrentPassword(e.target.value)}/>
+            <input type="password" placeholder='New Password' value={newPassword} onChange={e => setNewPassword(e.target.value)}/>
+            <input type="password" placeholder='Confirm New Password' value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)}/>
+            <button type="submit" className='btn-register'>Update Details</button>
+          </form>
+        </div>
+      </div>
     </section>
   )
 }
