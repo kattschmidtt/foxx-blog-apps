d00b66eb7576dd6d11b792983545c0ae4b03d029
Kat Schmidt <katschmidt201@gmail.com>
2024-03-27 22:34:03 -0400

    finished editPost function

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -64,10 +64,60 @@ const getPostById = async (req, res, next) => {
   }
 }
 const getPostByCategory = async (req, res, next) => {
-  res.json("Get Post By Category");
+  try {
+    const {category} = req.params;
+    const catPosts = await Post.find({category}).sort({createdAt: -1});
+    res.status(200).json(catPosts);
+  } catch(err) {
+    return next(new HttpError(err));
+  }
 }
 const editPost = async (req, res, next) => {
-  res.json("Edit Post");
+  try {
+    let fileName;
+    let newFileName;
+    let updatedPost;
+    const postId = req.params.id;
+    let {title, category, description} = req.body;
+
+    if(!title || !category || description.length < 12) {
+      return next(new HttpError("Fill in all fields.", 422));
+    }
+    if(!req.files) {
+      updatedPost = await Post.findByIdAndUpdate(postId, {title, category, description}, {new: true});
+    } else {
+      const oldPost = await Post.findById(postId);
+      fs.unlink(path.join(__dirname, '..', 'uploads', oldPost.thumbnail), async err => {
+        if(err) {
+          return next(new HttpError(err));
+        }
+      })
+
+      //edit thumbnail
+      const {thumbnail} = req.files;
+        if(thumbnail.size > 2000000) {
+          return next(new HttpError("Thumbnail too big. Should be less than 2mb"));
+        }
+        fileName = thumbnail.name;
+        let splitFileName = fileName.split('.');
+        newFileName = splitFileName[0] + uuid() + '.' + splitFileName[splitFileName.length - 1];
+        thumbnail.mv(path.join(__dirname, '..', 'uploads', newFileName), async err => {
+          if(err) {
+            return next(new HttpError(err))
+          }
+        })
+
+        updatedPost = await Post.findByIdAndUpdate(postId, {title, category, description, thumbnail: newFileName}, {new: true});
+    }
+    if(!updatedPost) {
+      return next(new HttpError("Couldn't update post", 400));
+    }
+
+    res.status(200).json(updatedPost);
+
+  } catch(err) {
+    return next(new HttpError(err));
+  }
 }
 
 module.exports = {createPost, getPosts, getPostById, getPostByCategory, editPost};
